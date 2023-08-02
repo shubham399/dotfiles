919a6d2264da767299688619b3f0602439dfdfc1
Shubham Kumar <hello@shubhkumar.in>
2023-08-02 09:05:27 +0530

    Updated Code to install homebrew if not installed.

--- run.sh
+++ run.sh
@@ -1,1 +1,26 @@
-/Users/shubham/Library/Python/3.11/bin/ansible-playbook --ask-become-pass main.yml
+#!/bin/bash
+
+check_command_installed() {
+    if command -v $1 >/dev/null 2>&1; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# Function to check if Homebrew is installed
+check_homebrew_installed() {
+    check_command_installed "brew"
+}
+
+# Check if we are on macOS
+if [[ "$(uname)" == "Darwin" ]]; then
+    if check_homebrew_installed; then
+        echo "Homebrew is already installed."
+    else
+        echo "Homebrew not found. Installing..."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+fi
+
+python3 -m ansible playbook main.yml
