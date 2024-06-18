c189df12a5979e995fcbfa35df926d8f13785a31
Shubham Kumar <hello@shubhkumar.in>
2024-06-18 09:41:01 +0530

    updated lazy-vim and zshrc to contain deno

--- configs/nvim/lazyvim/lua/config/lazy.lua
+++ configs/nvim/lazyvim/lua/config/lazy.lua
@@ -12,18 +12,17 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "nvim-lint" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    -- import/override with your plugins
-    { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-    { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.coding.1" },
+    { import = "lazyvim.plugins.extras.coding.tabnine" },
     { import = "lazyvim.plugins.extras.ui.alpha" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- import/override with your plugins
     { import = "plugins" },
   },
   defaults = {
@@ -51,5 +50,5 @@ require("lazy").setup({
         "zipPlugin",
       },
     },
-  }
+  },
 })
