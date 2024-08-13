470c40f47d87720da7c793262a72fd57b827de65
Shubham Kumar <hello@shubhkumar.in>
2024-08-13 14:50:07 +0530

    front_app.sh changes

--- configs/sketchybar/items/front_app.sh
+++ configs/sketchybar/items/front_app.sh
@@ -3,16 +3,14 @@
 
 # Command to retrieve the app icon path (this will depend on your system setup)
 # Example placeholder: retrieve the icon using a custom script or command
-GET_APP_ICON_SCRIPT='sketchybar --set $NAME icon=$(get_app_icon "$INFO")'
 
 # Front app label and icon update script
-FRONT_APP_SCRIPT='sketchybar --set $NAME icon="$APP_ICON" label="$INFO"'
+FRONT_APP_SCRIPT='sketchybar --set $NAME  label="$INFO"'
 
 sketchybar --add event window_focus \
     --add event windows_on_spaces \
     --add item front_app left \
     --set front_app script="$FRONT_APP_SCRIPT" \
-    icon.drawing=on \
     icon.font="$FONT:Bold:13.0" \
     icon.color="$ORANGE" \
     padding_left=0 \
