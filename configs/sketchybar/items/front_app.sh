ff96b4338449c4ae4c615f3ad6cd9cdb206f7f95
Shubham Kumar <hello@shubhkumar.in>
2024-08-13 14:16:24 +0530

    Some more changes

--- configs/sketchybar/items/front_app.sh
+++ configs/sketchybar/items/front_app.sh
@@ -1,27 +1,22 @@
 #!/usr/bin/env bash
-
 # shellcheck disable=SC2016
-FRONT_APP_SCRIPT='sketchybar --set $NAME label="$INFO"'
 
-sketchybar --add       event        window_focus                  \
-           --add       event        windows_on_spaces             \
-           --add       item         system.yabai left             \
-           --set       system.yabai script="$PLUGIN_DIR/yabai.sh" \
-                                    icon.font="$FONT:Normal:20.0"   \
-                                    label.drawing=off             \
-                                    icon.width=40                 \
-                                    icon="$YABAI_GRID"            \
-                                    icon.color="$ORANGE"          \
-                                    associated_display=active     \
-           --subscribe system.yabai window_focus                  \
-                                    windows_on_spaces             \
-                                    mouse.clicked                 \
-                                                                  \
-           --add       item         front_app left                \
-           --set       front_app    script="$FRONT_APP_SCRIPT"    \
-                                    icon.drawing=off              \
-                                    padding_left=0                \
-                                    label.color="$WHITE"          \
-                                    label.font="$FONT:Black:13.0" \
-                                    associated_display=active     \
-           --subscribe front_app    front_app_switched
+# Command to retrieve the app icon path (this will depend on your system setup)
+# Example placeholder: retrieve the icon using a custom script or command
+GET_APP_ICON_SCRIPT='sketchybar --set $NAME icon=$(get_app_icon "$INFO")'
+
+# Front app label and icon update script
+FRONT_APP_SCRIPT='sketchybar --set $NAME icon="$APP_ICON" label="$INFO"'
+
+sketchybar --add event window_focus \
+    --add event windows_on_spaces \
+    --add item front_app left \
+    --set front_app script="$FRONT_APP_SCRIPT" \
+    icon.drawing=on \
+    icon.font="$FONT:Bold:13.0" \
+    icon.color="$ORANGE" \
+    padding_left=0 \
+    label.color="$ORANGE" \
+    label.font="$FONT:Black:13.0" \
+    associated_display=active \
+    --subscribe front_app front_app_switched 
