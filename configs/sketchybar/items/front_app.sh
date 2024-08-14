8e6d67d3366a1b32fc605c3bee37dde8b8490fc2
Shubham Kumar <hello@shubhkumar.in>
2024-08-14 11:58:05 +0530

    Front_app

--- configs/sketchybar/items/front_app.sh
+++ configs/sketchybar/items/front_app.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2016
 
-# Command to retrieve the app icon path (this will depend on your system setup)
-# Example placeholder: retrieve the icon using a custom script or command
-
-# Front app label and icon update script
-FRONT_APP_SCRIPT='sketchybar --set $NAME  label="$INFO"'
 
 sketchybar --add event window_focus \
     --add event windows_on_spaces \
     --add item front_app left \
-    --set front_app script="$FRONT_APP_SCRIPT" \
+    --set front_app script="$PLUGIN_DIR/front_app.sh" \ \
     icon.font="$FONT:Bold:13.0" \
     icon.color="$ORANGE" \
+    icon="❓" \
     padding_left=0 \
     label.color="$ORANGE" \
     label.font="$FONT:Black:13.0" \
     associated_display=active \
-    --subscribe front_app front_app_switched 
+    --subscribe front_app front_app_switched
--- configs/sketchybar/plugins/front_app.sh
+++ configs/sketchybar/plugins/front_app.sh
@@ -1,6 +1,33 @@
 #!/usr/bin/env bash
 
+get_focused_app_icon_for_workspace() {
+  APP_NAME=$1
+
+  case "$APP_NAME" in
+  "Arc")
+    echo "🌐" # Globe icon for Safari
+    ;;
+  "Cursor")
+    echo "💻" # Laptop icon for Terminal
+    ;;
+  "WezTerm")
+    echo "💻" # Laptop icon for Terminal
+    ;;
+  "Finder")
+    echo "🗂" # Folder icon for Finder
+    ;;
+  "Slack")
+    echo "💬" # Chat bubble or any emoji for Slack
+    ;;
+  # Add more applications here
+  *)
+    echo "❓" # Default icon if app is not recognized
+    ;;
+  esac
+}
+
 case "$SENDER" in
-  "front_app_switched") sketchybar --set $NAME label="$INFO"
+"front_app_switched")
+  sketchybar --set $NAME label="$INFO" icon=$(get_focused_app_icon_for_workspace $INFO)
   ;;
 esac
