edd47f64aeb011db859b5980a0ae5164a1dbeb2e
Shubham Kumar <hello@shubhkumar.in>
2025-05-13 10:58:58 +0530

    absolute number line

---
+++ configs/nvim/lazyvim/lua/plugins/visual-multi.lua
@@ -0,0 +1,8 @@
+return {
+  "mg979/vim-visual-multi",
+  branch = "master",
+  init = function()
+    -- Optional: disable default mappings if you want full control
+    vim.g.VM_default_mappings = 0
+  end,
+}
