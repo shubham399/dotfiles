e139fde5539ae3fb0b4be457478bc091024e9d26
Shubham Kumar <hello@shubhkumar.in>
2026-04-24 16:08:20 +0530

    Updated somethings

--- configs/nvim/lazyvim/lua/config/autocmds.lua
+++ configs/nvim/lazyvim/lua/config/autocmds.lua
@@ -6,3 +6,19 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+--
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*.json",
+  callback = function(args)
+    local file = vim.api.nvim_buf_get_name(args.buf)
+    local ok, stats = pcall(vim.loop.fs_stat, file)
+
+    if ok and stats and stats.size > 500 * 1024 then
+      for _, client in ipairs(vim.lsp.get_clients({ bufnr = args.buf })) do
+        vim.lsp.stop_client(client.id)
+      end
+    end
+  end,
+})
--- configs/nvim/lazyvim/lua/config/options.lua
+++ configs/nvim/lazyvim/lua/config/options.lua
@@ -1,8 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.number = true -- Show absolute line numbers
-vim.opt.relativenumber = false -- Disable relative line numbers
+vim.opt.number = false -- Show absolute line numbers
+vim.opt.relativenumber = true -- Disable relative line numbers
 
 vim.filetype.add({
   pattern = {
