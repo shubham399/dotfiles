8969585405850b6af7fe8deeb002603206bfc3f3
Shubham Kumar <hello@shubhkumar.in>
2026-06-03 15:49:04 +0530

    Commit these changes

--- configs/nvim/lazyvim/lua/config/options.lua
+++ configs/nvim/lazyvim/lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.number = false -- Show absolute line numbers
+vim.opt.number = true -- Show absolute line numbers
 vim.opt.relativenumber = true -- Disable relative line numbers
 
 vim.filetype.add({
